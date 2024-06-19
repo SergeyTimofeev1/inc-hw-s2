21b8f8cb78346d35e2a5837596a99f7f76c7797d
SergeyTimofeev1 <timofeevbest@yandex.ru>
2024-06-19 19:01:30 +0300

    feat: add hw-12

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,51 @@
 import {UserType} from '../HW8'
+import User from "../User";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export type SortByNameType = { type: 'sort'; payload: SortValuesType }
+
+export type SortValuesType = 'up' |'down'
+
+
+export type CheckAgeAcType = { type: 'check'; payload: number }
+
+export type HomeWorkReducerActionType = SortByNameType | CheckAgeAcType
+
+const initialState: UserType[] = [
+    // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+]
+
+export const homeWorkReducer = (state: UserType[] = initialState, action: HomeWorkReducerActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const compareFn = (a:UserType, b:UserType) => {
+                return action.payload === 'down' ? a.age - b.age : b.age - a.age
+            }
 
-            return state // need to fix
+            return [...state].sort(compareFn)// need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state].filter(u => u.age > 18) // need to fix
         }
         default:
-            return state
+            return [...state]
     }
 }
+
+export const sortByNameAc = (payload: SortValuesType): SortByNameType => {
+    return {type: 'sort', payload}
+}
+
+export const checkAgeAc = (payload: number): CheckAgeAcType => {
+    return {type: 'check', payload}
+}
+
