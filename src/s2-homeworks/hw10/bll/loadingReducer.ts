f8ab1cf265c0220b490ec006894dec225b28c9e0
SergeyTimofeev1 <timofeevbest@yandex.ru>
2024-07-22 16:00:31 +0300

    feat: add hw-14

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,15 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export type InitialStateType = typeof initState
+export type ActionsType = LoadingActionType
+
+export const loadingReducer = (state = initState, action: ActionsType): InitialStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
         default:
             return state
     }
